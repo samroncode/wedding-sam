66f570117206e309b9bf25c42866ad2bf674180e
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2023-09-16 21:42:32 +0200

    Add svh to main to prevent mobile styling bugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,10 @@ import CountdownSection from "./sections/CountdownSection";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center p-12 lg:p-24">
+      <main
+        className="flex h- lg:min-h-screen flex-col items-center justify-center p-12 lg:p-24"
+        style={{ height: "100svh" }}
+      >
         <div className="z-10 max-w-5xl w-full flex-col items-center justify-center font-mono text-sm lg:flex">
           {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
