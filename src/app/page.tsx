d7fe09dd57efdf30aa3e54d939aff3ce9992feb2
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2023-09-16 20:53:49 +0200

    Add Head component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,18 @@ import ContactForm from "./components/Form";
 import Countdown from "./components/Countdown";
 import RSVPSection from "./sections/RSVPSection";
 import CountdownSection from "./sections/CountdownSection";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full flex-col items-center justify-center font-mono text-sm lg:flex">
-        {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <>
+      <Head>
+        <title>Hanna & Anders</title>
+        <meta property="og:title" content="Hanna & Anders" key="title" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full flex-col items-center justify-center font-mono text-sm lg:flex">
+          {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -25,20 +31,21 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>
+            </a>
         </div> */}
 
-        {/* HERO - with image */}
-        {/* RSVP */}
-        {/* Contact Toastmaster */}
-        {/* Map + Instructions */}
-        {/* Countdown */}
-        {/* Children Information */}
-        {/* Dresscode */}
+          {/* HERO - with image */}
+          {/* RSVP */}
+          {/* Contact Toastmaster */}
+          {/* Map + Instructions */}
+          {/* Countdown */}
+          {/* Children Information */}
+          {/* Dresscode */}
 
-        <CountdownSection />
-        {/* <RSVPSection /> */}
-      </div>
-    </main>
+          <CountdownSection />
+          {/* <RSVPSection /> */}
+        </div>
+      </main>
+    </>
   );
 }
