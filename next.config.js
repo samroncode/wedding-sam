11d235a414a5fde73c7a27f440fcdd4e0e52cd43
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2023-09-18 00:55:48 +0200

    Remove logs and try new env workflow

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 
 module.exports = {
   output: "export",
-  images: { loader: "akamai", path: "" },
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY:
-      process.env.NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
-    NEXT_PUBLIC_SPREADSHEET_ID: process.env.NEXT_PUBLIC_SPREADSHEET_ID,
-    NEXT_PUBLIC_SHEET_ID: process.env.NEXT_PUBLIC_SHEET_ID,
-    NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_EMAIL:
-      process.env.NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_EMAIL
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
@@ -19,5 +10,13 @@ module.exports = {
       config.resolve.fallback.child_process = false;
     }
     return config;
+  },
+  env: {
+    GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY:
+      process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    NEXT_PUBLIC_SPREADSHEET_ID: process.env.NEXT_PUBLIC_SPREADSHEET_ID,
+    NEXT_PUBLIC_SHEET_ID: process.env.NEXT_PUBLIC_SHEET_ID,
+    NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_EMAIL:
+      process.env.NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_EMAIL
   }
 };
