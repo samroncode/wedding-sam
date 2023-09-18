dfd30aca3e4efdbba69be53a2514e81410cc6f41
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2023-09-18 18:16:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  // output: "export",
+  output: "export",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
